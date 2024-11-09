4f5a4cd144f757db9ef86c8d46b0eafb8b056f1b
rickyega <rickyega@yahoo.ne.jp>
2024-11-09 20:42:53 +0100

    change table loot table

--- src/main/java/ricky/terrariamod/util/ModLootTableModifiers.java
+++ src/main/java/ricky/terrariamod/util/ModLootTableModifiers.java
@@ -12,28 +12,91 @@ import ricky.terrariamod.item.ModItems;
 
 public class ModLootTableModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID =
-            new Identifier("minecraft", "chest/jungle_temple");
+            new Identifier("minecraft", "chests/jungle_temple");
+    private static final Identifier DESERT_PYRAMID_ID =
+            new Identifier("minecraft", "chests/desert_pyramid");
+    private static final Identifier ANCIENT_CITY_ID =
+            new Identifier("minecraft", "chests/ancient_city");
+    private static final Identifier BURIED_TREASURE_ID =
+            new Identifier("minecraft", "chests/buried_treasure");
+    private static final Identifier BASTION_TREASURE_ID =
+            new Identifier("minecraft", "chests/bastion_treasure");
+    private static final Identifier FISHING_LOOT_TABLE_ID =
+            new Identifier("minecraft", "gameplay/fishing/treasure");
     private static final Identifier CREEPER_ID =
             new Identifier("minecraft", "entities/creeper");
+    private static final Identifier PIGLIN_BRUTE_ID =
+            new Identifier("minecraft", "entities/piglin_brute");
 
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            //マジックミラーをジャングル寺院のチェストに追加
             if(JUNGLE_TEMPLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .rolls(ConstantLootNumberProvider.create(1))//最大個数
+                        .conditionally(RandomChanceLootCondition.builder(0.5f)) // 確率
+                        .with(ItemEntry.builder(ModItems.MAGIC_MIRROR))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            //マジックミラーをピラミッドのチェストに追加
+            if(DESERT_PYRAMID_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))//最大個数
+                        .conditionally(RandomChanceLootCondition.builder(0.16f)) // 確率
+                        .with(ItemEntry.builder(ModItems.MAGIC_MIRROR))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            //金属探知機を古代都市のチェストに追加
+            if(ANCIENT_CITY_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))//最大個数
+                        .conditionally(RandomChanceLootCondition.builder(0.5f)) // 確率
                         .with(ItemEntry.builder(ModItems.MetalDetectorItem))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
-
-            if(CREEPER_ID.equals(id)) {
+            //無限水バケツを埋もれた宝箱に追加
+            if(BURIED_TREASURE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f))
-                        .with(ItemEntry.builder(ModItems.MetalDetectorItem))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.INFINITE_WATER_BUCKET))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            //無限水バケツを釣りに追加
+            if(FISHING_LOOT_TABLE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.01f))
+                        .with(ItemEntry.builder(ModItems.INFINITE_WATER_BUCKET))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            //無限溶岩バケツをピグリン要塞に追加
+            if(BASTION_TREASURE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(ModItems.INFINITE_LAVA_BUCKET))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            //無限溶岩バケツをピグリンブルートのドロップアイテムに追加
+            if(PIGLIN_BRUTE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.3f))
+                        .with(ItemEntry.builder(ModItems.INFINITE_LAVA_BUCKET))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
