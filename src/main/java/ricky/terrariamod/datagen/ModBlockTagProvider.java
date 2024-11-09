56adbe82a9277cc738aa4e44228713926b591ed3
rickyega <rickyega@yahoo.ne.jp>
2024-11-09 18:05:39 +0100

    fix

--- src/main/java/ricky/terrariamod/datagen/ModBlockTagProvider.java
+++ src/main/java/ricky/terrariamod/datagen/ModBlockTagProvider.java
@@ -2,6 +2,7 @@ package ricky.terrariamod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
@@ -22,14 +23,19 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS)
                 .add(ModBlocks.COBALT_ORE)
                 .add(ModBlocks.DEEPSLATE_COBALT_ORE)
-                .forceAddTag(BlockTags.GOLD_ORES)
-                .forceAddTag(BlockTags.EMERALD_ORES)
-                .forceAddTag(BlockTags.REDSTONE_ORES)
-                .forceAddTag(BlockTags.LAPIS_ORES)
-                .forceAddTag(BlockTags.DIAMOND_ORES)
-                .forceAddTag(BlockTags.IRON_ORES)
-                .forceAddTag(BlockTags.COPPER_ORES)
-                .forceAddTag(BlockTags.COAL_ORES);
+                .add(ModBlocks.ORICHALCUM_ORE)
+                .add(ModBlocks.DEEPSLATE_ORICHALCUM_ORE)
+                .add(ModBlocks.ADAMANTITE_ORE)
+                .add(ModBlocks.DEEPSLATE_ADAMANTITE_ORE)
+                .add(ModBlocks.HELLSTONE_ORE);
+//                .forceAddTag(BlockTags.GOLD_ORES)
+//                .forceAddTag(BlockTags.EMERALD_ORES)
+//                .forceAddTag(BlockTags.REDSTONE_ORES)
+//                .forceAddTag(BlockTags.LAPIS_ORES)
+//                .forceAddTag(BlockTags.DIAMOND_ORES)
+//                .forceAddTag(BlockTags.IRON_ORES)
+//                .forceAddTag(BlockTags.COPPER_ORES)
+//                .forceAddTag(BlockTags.COAL_ORES);
 
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
                 .add(ModBlocks.COBALT_ORE)
