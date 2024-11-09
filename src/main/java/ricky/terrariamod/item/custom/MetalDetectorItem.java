9858451507ff3d189eb23aa5d2255d27eacb4ce2
rickyega <rickyega@yahoo.ne.jp>
2024-11-09 13:32:18 +0100

    fix metal detector

--- src/main/java/ricky/terrariamod/item/custom/MetalDetectorItem.java
+++ src/main/java/ricky/terrariamod/item/custom/MetalDetectorItem.java
@@ -3,12 +3,11 @@ package ricky.terrariamod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -17,43 +16,54 @@ import ricky.terrariamod.util.ModTags;
 import java.util.List;
 
 public class MetalDetectorItem extends Item {
+
     public MetalDetectorItem(Settings settings) {
         super(settings);
     }
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        if(context.getWorld().isClient){
-            BlockPos positionClicked = context.getBlockPos();
-            PlayerEntity player = context.getPlayer();
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        // クライアントサイドのみで処理
+        if (world.isClient && entity instanceof PlayerEntity player) {
+            // プレイヤーを中心に 5 ブロックの範囲をスキャン
             boolean foundBlock = false;
+            BlockPos playerPos = player.getBlockPos();
 
-            for(int i=0; i<=positionClicked.getY()+64;i++){
-                BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
+            for (int x = -5; x <= 5; x++) {
+                for (int y = -5; y <= 5; y++) {
+                    for (int z = -5; z <= 5; z++) {
+                        BlockPos scanPos = playerPos.add(x, y, z);
+                        BlockState state = world.getBlockState(scanPos);
 
-                if(isValuableBlock(state)){
-                    outValuableCoordinates(positionClicked.down(i), player, state.getBlock());
-                    foundBlock = true;
-                    break;
+                        // 鉱石ブロックかどうか判定
+                        if (isValuableBlock(state)) {
+                            outValuableCoordinates(scanPos, player, state.getBlock());
+                            foundBlock = true;
+                            break;
+                        }
+                    }
+                    if (foundBlock) break;
                 }
+                if (foundBlock) break;
             }
 
-            if(!foundBlock){
-                player.sendMessage(Text.literal("No Valuables Found!"));
+            // 鉱石が見つからなかった場合
+            if (!foundBlock) {
+                player.sendMessage(Text.literal("No Valuables Nearby!"), true);
             }
         }
-        context.getStack().damage(1,context.getPlayer(),
-                playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
-        return ActionResult.SUCCESS;
-    }
-    private void outValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
-        player.sendMessage(Text.literal("Found "+ block.asItem().getName().getString() + " at (" +
-                blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"),false);
     }
 
-    private boolean isValuableBlock(BlockState state){
+
+    private void outValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
+        player.sendMessage(Text.literal("Found " + block.asItem().getName().getString() + " at (" +
+                blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), true);
+    }
+
+    private boolean isValuableBlock(BlockState state) {
         return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
     }
+
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.translatable("tooltip.terrariamod.metal_detector.tooltip"));
