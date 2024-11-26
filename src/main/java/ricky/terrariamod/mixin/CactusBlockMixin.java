92936247868b47a84c3e033d5fe94be8126c97fb
rickyega <rickyega@yahoo.ne.jp>
2024-11-26 16:22:38 +0100

    use left click to shoot shotgun

--- src/main/java/ricky/terrariamod/mixin/CactusBlockMixin.java
+++ src/main/java/ricky/terrariamod/mixin/CactusBlockMixin.java
@@ -1,31 +1,23 @@
 package ricky.terrariamod.mixin;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ricky.terrariamod.block.ModBlocks;
-
-import java.util.Iterator;
-
 @Mixin(CactusBlock.class)
 public abstract class CactusBlockMixin {
-
     @Inject(method = "canPlaceAt", at = @At("HEAD"), cancellable = true)
     public void canPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        Iterator var4 = Direction.Type.HORIZONTAL.iterator();
+        System.out.println("cactus mixin method has been called!");  // デバッグ用メッセージ
         Block blockBelow = world.getBlockState(pos.down()).getBlock();
         // `Ebon Sand` の上にもサボテンが生成可能にする
-        if (!var4.hasNext()) {
-            if ((blockBelow == ModBlocks.EBON_SAND || blockBelow == ModBlocks.CRIM_SAND || blockBelow == ModBlocks.PEARL_SAND)){
-                cir.setReturnValue(true);
-            }
+        if (blockBelow == ModBlocks.EBON_SAND || blockBelow == ModBlocks.CRIM_SAND || blockBelow == ModBlocks.PEARL_SAND) {
+            cir.setReturnValue(true);
         }
     }
 }
--- src/main/java/ricky/terrariamod/mixin/LivingEntityMixin.java
+++ src/main/java/ricky/terrariamod/mixin/LivingEntityMixin.java
@@ -1,47 +1,66 @@
 package ricky.terrariamod.mixin;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import ricky.terrariamod.effect.ModEffects;
+import ricky.terrariamod.item.bows.ShotgunItem;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {//TODO 反転してない
-
-    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
-    public void travel(Vec3d movementInput, CallbackInfo info) {
+    @Inject(method = "swingHand", at = @At("HEAD"), cancellable = true)
+    private void onHandleInputEvents(CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
 
-        // Confusedエフェクトが適用されているか確認
-        if (entity.hasStatusEffect(ModEffects.CONFUSED)) {
-            // 移動方向を反転
-            Vec3d reversedInput = new Vec3d(
-                    -movementInput.x,  // 左右反転
-                    movementInput.y,   // 上下はそのまま
-                    -movementInput.z   // 前後反転
-            );
+        // entity が PlayerEntity のインスタンスか確認
+        if (entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity; // プレイヤーとしてキャスト
 
-            // 移動処理を実行
-            this.customTravel(entity, reversedInput);
+            // プレイヤーが Shotgun を持っているか確認
+            if (player.getMainHandStack().getItem() instanceof ShotgunItem) {
+                System.out.println("Mixin : Shotgun detected!");
 
-            // オリジナルのtravelメソッドを無効化
-            info.cancel();
+                // ショットガンを発射
+                ShotgunItem shotgun = (ShotgunItem) player.getMainHandStack().getItem();
+                shotgun.use(player.getWorld(), player, Hand.MAIN_HAND);
+
+                ci.cancel(); // 通常の手を振る動作をキャンセル
+            }
         }
     }
 
-    private void customTravel(LivingEntity entity, Vec3d movementInput) {
-        // 流体判定と速度適用
-        boolean isInWater = entity.updateMovementInFluid(FluidTags.WATER, 0.014); // 水中での動き
-        boolean isInLava = entity.updateMovementInFluid(FluidTags.LAVA, 0.014);  // 溶岩での動き
-
-        if (!isInWater && !isInLava) {
-            // 通常の速度処理を行う（例: 地上の場合）
-            entity.setVelocity(entity.getVelocity().add(movementInput));
-        }
-    }
+//    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
+//    public void travel(Vec3d movementInput, CallbackInfo info) {
+//        LivingEntity entity = (LivingEntity) (Object) this;
+//
+//        // Confusedエフェクトが適用されているか確認
+//        if (entity.hasStatusEffect(ModEffects.CONFUSED)) {
+//            // 移動方向を反転
+//            Vec3d reversedInput = new Vec3d(
+//                    -movementInput.x,  // 左右反転
+//                    movementInput.y,   // 上下はそのまま
+//                    -movementInput.z   // 前後反転
+//            );
+//
+//            // 移動処理を実行
+//            this.customTravel(entity, reversedInput);
+//
+//            // オリジナルのtravelメソッドを無効化
+//            info.cancel();
+//        }
+//    }
+//
+//    private void customTravel(LivingEntity entity, Vec3d movementInput) {
+//        // 流体判定と速度適用
+//        boolean isInWater = entity.updateMovementInFluid(FluidTags.WATER, 0.014); // 水中での動き
+//        boolean isInLava = entity.updateMovementInFluid(FluidTags.LAVA, 0.014);  // 溶岩での動き
+//
+//        if (!isInWater && !isInLava) {
+//            // 通常の速度処理を行う（例: 地上の場合）
+//            entity.setVelocity(entity.getVelocity().add(movementInput));
+//        }
+//    }
 }
