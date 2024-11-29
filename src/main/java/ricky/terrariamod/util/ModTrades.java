b58b8709a2cedd05137b38d6056c11df7ce35551
rickyega <rickyega@yahoo.ne.jp>
2024-11-29 14:16:17 +0100

    fix trade

--- src/main/java/ricky/terrariamod/util/ModTrades.java
+++ src/main/java/ricky/terrariamod/util/ModTrades.java
@@ -11,28 +11,28 @@ public class ModTrades {
     public static void registerCustomTrades() {
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 5,
                 factories -> factories.add((entity, random) -> new TradeOffer(
-                        new ItemStack(Items.EMERALD, 10),
+                        new ItemStack(Items.EMERALD_BLOCK, 10),
                         new ItemStack(ModItems.SHOTGUN, 1),
                         2, 10, 0.2f)));
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH,5,
                 factories -> factories.add((entity, random) -> new TradeOffer(
-                            new ItemStack(Items.EMERALD, 10),
+                            new ItemStack(Items.EMERALD_BLOCK, 10),
                             new ItemStack(ModItems.SNIPER_RIFLE,1),
                             2,10,0.2f)));
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH,5,
                 factories -> factories.add((entity, random) -> new TradeOffer(
-                            new ItemStack(Items.EMERALD, 10),
+                            new ItemStack(Items.EMERALD_BLOCK, 10),
                             new ItemStack(ModItems.ROCKET_LAUNCHER,1),
                             2,10,0.2f)));
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH,4,
                 factories -> factories.add((entity, random) -> new TradeOffer(
                             new ItemStack(Items.EMERALD, 10),
-                            new ItemStack(ModItems.ROCKET,1),
+                            new ItemStack(ModItems.ROCKET,5),
                             10,10,0.2f)));
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH,4,
                 factories -> factories.add((entity, random) -> new TradeOffer(
                             new ItemStack(Items.EMERALD, 10),
-                            new ItemStack(ModItems.MUSKET_BALL,1),
+                            new ItemStack(ModItems.MUSKET_BALL,10),
                             10,10,0.2f)));
     }
 }
