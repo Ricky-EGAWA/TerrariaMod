7eccad082dff5f4148403ea52f92b46bf0445006
rickyega <rickyega@yahoo.ne.jp>
2024-12-08 17:05:54 +0100

    clear code MagicBallEntity

--- src/main/java/ricky/terrariamod/entity/magic/MagicBallEntity.java
+++ src/main/java/ricky/terrariamod/entity/magic/MagicBallEntity.java
@@ -1,25 +1,15 @@
 package ricky.terrariamod.entity.magic;
 
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -27,11 +17,8 @@ import ricky.terrariamod.entity.ModEntities;
 import ricky.terrariamod.item.ModItems;
 
 public class MagicBallEntity extends PersistentProjectileEntity {
-    private static final TrackedData<Byte> LOYALTY;
-    private static final TrackedData<Boolean> BALL;
     private ItemStack tridentStack;
     private boolean dealtDamage;
-    public int returnTimer;
 
     // 寿命を管理するタイマー（初期値: 100 ticks = 5秒）
     private int lifeTime = 100;
@@ -45,14 +32,10 @@ public class MagicBallEntity extends PersistentProjectileEntity {
         super(ModEntities.AMETHYST_BALL, owner, world);
         this.tridentStack = new ItemStack(ModItems.AMETHYST_STAFF);
         this.tridentStack = stack.copy();
-        this.dataTracker.set(LOYALTY, (byte) EnchantmentHelper.getLoyalty(stack));
-        this.dataTracker.set(BALL, stack.hasGlint());
     }
 
     protected void initDataTracker() {
         super.initDataTracker();
-        this.dataTracker.startTracking(LOYALTY, (byte)0);
-        this.dataTracker.startTracking(BALL, false);
     }
 
     public void tick() {
@@ -74,33 +57,6 @@ public class MagicBallEntity extends PersistentProjectileEntity {
 
         if (this.inGroundTime > 4) {
             this.dealtDamage = true;
-        }
-
-        Entity entity = this.getOwner();
-        int i = this.dataTracker.get(LOYALTY);
-        if (i > 0 && (this.dealtDamage || this.isNoClip()) && entity != null) {
-            if (!this.isOwnerAlive()) {
-                if (!this.getWorld().isClient && this.pickupType == PickupPermission.ALLOWED) {
-                    this.dropStack(this.asItemStack(), 0.1F);
-                }
-
-                this.discard();
-            } else {
-                this.setNoClip(true);
-                Vec3d vec3d = entity.getEyePos().subtract(this.getPos());
-                this.setPos(this.getX(), this.getY() + vec3d.y * 0.015 * (double)i, this.getZ());
-                if (this.getWorld().isClient) {
-                    this.lastRenderY = this.getY();
-                }
-
-                double d = 0.05 * (double)i;
-                this.setVelocity(this.getVelocity().multiply(0.95).add(vec3d.normalize().multiply(d)));
-                if (this.returnTimer == 0) {
-                    this.playSound(SoundEvents.ITEM_TRIDENT_RETURN, 10.0F, 1.0F);
-                }
-
-                ++this.returnTimer;
-            }
         }
     }
 
@@ -136,6 +92,7 @@ public class MagicBallEntity extends PersistentProjectileEntity {
             }
         }
     }
+
     private void spawnCircleParticles() {
         World world = this.getWorld();
         if (world.isClient) { // クライアントサイドでのみ実行
@@ -172,16 +129,6 @@ public class MagicBallEntity extends PersistentProjectileEntity {
         }
     }
 
-
-    private boolean isOwnerAlive() {
-        Entity entity = this.getOwner();
-        if (entity != null && entity.isAlive()) {
-            return !(entity instanceof ServerPlayerEntity) || !entity.isSpectator();
-        } else {
-            return false;
-        }
-    }
-
     protected ItemStack asItemStack() {
         return this.tridentStack.copy();
     }
@@ -193,66 +140,30 @@ public class MagicBallEntity extends PersistentProjectileEntity {
 
     protected void onEntityHit(EntityHitResult entityHitResult) {
         Entity entity = entityHitResult.getEntity();
-        float f = 8.0F;
-        if (entity instanceof LivingEntity livingEntity) {
-            f += EnchantmentHelper.getAttackDamage(this.tridentStack, livingEntity.getGroup());
-        }
+        float damage = 8.0F;
 
-        Entity entity2 = this.getOwner();
-        DamageSource damageSource = this.getDamageSources().trident(this, (entity2 == null ? this : entity2));
+        DamageSource damageSource = this.getDamageSources().trident(this, this.getOwner());
         this.dealtDamage = true;
-        SoundEvent soundEvent = SoundEvents.ITEM_TRIDENT_HIT;
-        if (entity.damage(damageSource, f)) {
-            if (entity.getType() == EntityType.ENDERMAN) {
-                return;
-            }
 
-            if (entity instanceof LivingEntity) {
-                LivingEntity livingEntity2 = (LivingEntity)entity;
-                if (entity2 instanceof LivingEntity) {
-                    EnchantmentHelper.onUserDamaged(livingEntity2, entity2);
-                    EnchantmentHelper.onTargetDamaged((LivingEntity)entity2, livingEntity2);
-                }
-
-                this.onHit(livingEntity2);
+        if (entity.damage(damageSource, damage)) {
+            if (entity instanceof LivingEntity livingEntity) {
+                this.onHit(livingEntity);
             }
         }
 
         this.setVelocity(this.getVelocity().multiply(-0.01, -0.1, -0.01));
-        float g = 1.0F;
-        if (this.getWorld() instanceof ServerWorld && this.getWorld().isThundering() && this.hasChanneling()) {
-            BlockPos blockPos = entity.getBlockPos();
-            if (this.getWorld().isSkyVisible(blockPos)) {
-                LightningEntity lightningEntity = EntityType.LIGHTNING_BOLT.create(this.getWorld());
-                if (lightningEntity != null) {
-                    lightningEntity.refreshPositionAfterTeleport(Vec3d.ofBottomCenter(blockPos));
-                    lightningEntity.setChanneler(entity2 instanceof ServerPlayerEntity ? (ServerPlayerEntity)entity2 : null);
-                    this.getWorld().spawnEntity(lightningEntity);
-                    soundEvent = SoundEvents.ITEM_TRIDENT_THUNDER;
-                    g = 5.0F;
-                }
-            }
-        }
-
-        this.playSound(soundEvent, g, 1.0F);
-    }
-
-    public boolean hasChanneling() {
-        return EnchantmentHelper.hasChanneling(this.tridentStack);
     }
 
     protected boolean tryPickup(PlayerEntity player) {
-        return super.tryPickup(player) || this.isNoClip() && this.isOwner(player) && player.getInventory().insertStack(this.asItemStack());
+        return super.tryPickup(player) || player.getInventory().insertStack(this.asItemStack());
     }
 
-    protected SoundEvent getHitSound() {
-        return SoundEvents.ITEM_TRIDENT_HIT_GROUND;
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        this.discard();
     }
 
-    public void onPlayerCollision(PlayerEntity player) {
-        if (this.isOwner(player) || this.getOwner() == null) {
-            super.onPlayerCollision(player);
-        }
+    protected void onHit(LivingEntity target) {
+        this.discard();
     }
 
     public void readCustomDataFromNbt(NbtCompound nbt) {
@@ -262,7 +173,6 @@ public class MagicBallEntity extends PersistentProjectileEntity {
         }
 
         this.dealtDamage = nbt.getBoolean("DealtDamage");
-        this.dataTracker.set(LOYALTY, (byte)EnchantmentHelper.getLoyalty(this.tridentStack));
     }
 
     public void writeCustomDataToNbt(NbtCompound nbt) {
@@ -271,33 +181,7 @@ public class MagicBallEntity extends PersistentProjectileEntity {
         nbt.putBoolean("DealtDamage", this.dealtDamage);
     }
 
-    public void age() {
-        int i = this.dataTracker.get(LOYALTY);
-        if (this.pickupType != PickupPermission.ALLOWED || i <= 0) {
-            super.age();
-        }
-    }
-
-    protected float getDragInWater() {
-        return 0.99F;
-    }
-
     public boolean shouldRender(double cameraX, double cameraY, double cameraZ) {
         return true;
-    }
-
-    static {
-        LOYALTY = DataTracker.registerData(net.minecraft.entity.projectile.TridentEntity.class, TrackedDataHandlerRegistry.BYTE);
-        BALL = DataTracker.registerData(net.minecraft.entity.projectile.TridentEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
-    }
-
-    @Override
-    protected void onBlockHit(BlockHitResult blockHitResult) {
-        this.discard();
-    }
-
-    @Override
-    protected void onHit(LivingEntity target) {
-        this.discard();
     }
 }
